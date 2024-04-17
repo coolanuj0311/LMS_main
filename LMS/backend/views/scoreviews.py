d343a652b507294453250e05a54dad59cd15ab6f
Anuj Pratap Singh <126389158+coolanuj0311@users.noreply.github.com>
2024-04-17 18:11:48 +0530

    Update scoreviews.py

--- LMS/backend/views/scoreviews.py
+++ LMS/backend/views/scoreviews.py
@@ -86,10 +86,6 @@ class UpdateCompleteQuizCountView(ClientAdminMixin, APIView):
 
     def post(self, request):
         try:
-            # Check if the user has client admin privileges
-            # if not self.has_client_admin_privileges(request):
-            #     return JsonResponse({"error": "You do not have permission to access this resource"}, status=403)
-
             course_id = request.data.get('course_id')
             user_id = request.data.get('user_id')
 
@@ -109,15 +105,16 @@ class UpdateCompleteQuizCountView(ClientAdminMixin, APIView):
             quiz_score.completed_quiz_count = completed_quizzes_count
             quiz_score.save()
 
-            return Response({'message': 'Completed quiz count updated successfully'}, status=status.HTTP_200_OK)
+            # Serialize the QuizScore object
+            serializer = QuizScoreSerializer(quiz_score)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-
         
 
-class CreateQuizScoreView(ClientAdminMixin,APIView):
+class CreateQuizScoreView(ClientAdminMixin, APIView):
     """
     allowed for client admin
     POST request
@@ -134,32 +131,25 @@ class CreateQuizScoreView(ClientAdminMixin,APIView):
         total_score_per_course = (default=0)
     """
     permission_classes = [ClientAdminPermission]
+    
     def post(self, request):
         try:
-            # Check if the user has client admin privileges
-            # if not self.has_client_admin_privileges(request):
-            #     return JsonResponse({"error": "You do not have permission to access this resource"}, status=403)
             course_ids = request.data.get('course_id', [])
             user_ids = request.data.get('user_id', [])
 
-            # Validate request data
             if not course_ids or not user_ids:
                 return Response({'error': 'course_id and user_id lists are required'}, status=status.HTTP_400_BAD_REQUEST)
 
-            # Create quiz score records
             quiz_scores = []
             for course_id in course_ids:
-                # Calculate total quizzes for the course
                 total_quizzes_per_course = self.get_total_quizzes_per_course(course_id)
 
                 for user_id in user_ids:
-                    # Check if QuizScore instance already exists for this user and course
                     existing_score = QuizScore.objects.filter(course_id=course_id, enrolled_user_id=user_id).first()
 
                     if existing_score:
-                        continue  # Skip creation if QuizScore instance already exists
+                        continue
 
-                    # Create new QuizScore instance
                     quiz_score = QuizScore(
                         enrolled_user_id=user_id,
                         course_id=course_id,
@@ -172,21 +162,20 @@ class CreateQuizScoreView(ClientAdminMixin,APIView):
                     )
                     quiz_scores.append(quiz_score)
 
-            # Save quiz score records to the database
             QuizScore.objects.bulk_create(quiz_scores)
 
-            return Response({'message': 'Quiz scores created successfully'}, status=status.HTTP_201_CREATED)
+            serializer = QuizScoreSerializer(quiz_scores, many=True)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def get_total_quizzes_per_course(self, course_id):
         try:
-        # Count the number of quizzes associated with the given course_id
             total_quizzes = CourseStructure.objects.filter(course_id=course_id, content_type='quiz', active=True).count()
             return total_quizzes
         except Exception as e:
-        # Handle exceptions if any
-          return 0  # Return 0 in case of error
+            return 0
+
 
 
 
