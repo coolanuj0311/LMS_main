2668358c0579bb88d4e1e00e8c59d12e6c669b3e
Anuj Pratap Singh <126389158+coolanuj0311@users.noreply.github.com>
2024-04-17 18:03:03 +0530

    Update scoreserializers.py

--- LMS/backend/serializers/scoreserializers.py
+++ LMS/backend/serializers/scoreserializers.py
@@ -1,7 +1,11 @@
+# serializers.py
+
 from rest_framework import serializers
 from backend.models.allmodels import CourseCompletionStatusPerUser
 from rest_framework import serializers
 from backend.models.allmodels import QuizAttemptHistory, QuizScore
+from rest_framework import serializers
+from backend.models.allmodels import QuizScore
 
 
 class CourseCompletionStatusSerializer(serializers.ModelSerializer):
@@ -10,12 +14,9 @@ class CourseCompletionStatusSerializer(serializers.ModelSerializer):
         fields = ['enrolled_user_id', 'course_id', 'status', 'created_at']
 
 
-class QuizAttemptHistorySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = QuizAttemptHistory
-        fields = ['course_id', 'enrolled_user_id', 'quiz_id', 'complete']
+
 
 class QuizScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuizScore
-        fields = ['course_id', 'enrolled_user_id', 'completed_quiz_count']
+        fields = ['course_id', 'enrolled_user_id', 'total_score_per_course']
