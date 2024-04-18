9bd7d3a3a6823b66eeb379b607ead95ccdc0607b
Anuj Pratap Singh <126389158+coolanuj0311@users.noreply.github.com>
2024-04-18 11:30:21 +0530

    Update scoreserializers.py

--- LMS/backend/serializers/scoreserializers.py
+++ LMS/backend/serializers/scoreserializers.py
@@ -1,22 +1,29 @@
-# serializers.py
-
 from rest_framework import serializers
 from backend.models.allmodels import CourseCompletionStatusPerUser
-from rest_framework import serializers
-from backend.models.allmodels import QuizAttemptHistory, QuizScore
-from rest_framework import serializers
 from backend.models.allmodels import QuizScore
-
 
 class CourseCompletionStatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = CourseCompletionStatusPerUser
         fields = ['enrolled_user_id', 'course_id', 'status', 'created_at']
 
-
-
+    def validate(self, data):
+        # Add custom validation logic here
+        # For example, you can check if 'status' is a valid value
+        status = data.get('status')
+        if status not in ['completed','in_progress','not_started']:
+            raise serializers.ValidationError("Status must be 'completed' or 'in_progress'or'not_started'")
+        return data
 
 class QuizScoreSerializer(serializers.ModelSerializer):
     class Meta:
         model = QuizScore
-        fields = ['course_id', 'enrolled_user_id', 'total_score_per_course']
+        fields = ['enrolled_user_id', 'course_id', 'total_quizzes_per_course', 'completed_quiz_count', 'total_score_per_course', 'created_at', 'updated_at', 'active']
+
+    def validate(self, data):
+        # Add custom validation logic here
+        # For example, you can check if 'total_quizzes_per_course' is a positive integer
+        total_quizzes_per_course = data.get('total_quizzes_per_course')
+        if total_quizzes_per_course is not None and total_quizzes_per_course <= 0:
+            raise serializers.ValidationError("Total quizzes must be a positive integer")
+        return data
